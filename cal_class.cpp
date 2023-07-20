7331a2c1de4e34eef2703c9b210ce10842b447c4
Brijeshvekariya <bvekariya54@gmail.com>
2023-07-20 14:32:25 +0530

    first

---
+++ cal_class.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class calculator
+{
+	public :
+		void add(int a,int b)
+		{
+			cout<<"\n\nAddition of "<<a<<" and "<<b<<" is : "<<a+b<<endl;
+		}
+		void sub(int a,int b)
+		{
+			cout<<"Subtraction of "<<a<<" and "<<b<<" is : "<<a-b<<endl;
+		}
+		void mul(int a,int b)
+		{
+			cout<<"Multiplication of "<<a<<" and "<<b<<" is : "<<a*b<<endl;
+		}
+		void div(int a,int b)
+		{
+			cout<<"Division of "<<a<<" and "<<b<<" is : "<<a/b<<endl;
+		}
+		void mod(int a,int b)
+		{
+			cout<<"Modulo/Remainder of "<<a<<" and "<<b<<" is : "<<a%b<<endl;
+		}
+};
+
+int main()
+{
+	int a,b;
+	cout<<"Welcome to Calculator"<<endl;
+	cout<<"\n\nEnter first number : ";
+	cin>>a;
+	cout<<"Enter Second number : ";
+	cin>>b;
+	calculator c;
+	c.add(a,b);
+	c.sub(a,b);
+	c.mul(a,b);
+	c.div(a,b);
+	c.mod(a,b);
+}
